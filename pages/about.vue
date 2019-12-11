61d845fdf40d6929d1a5cedc69cb3715d2c177a7
Victorio Berra <tory.berra@wustl.edu>
2019-12-11 11:08:28 -0600

    Added some additional info to about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,7 @@
   <v-layout>
     <v-flex class="text-center">
       <blockquote class="blockquote">
-        GUI for: https://patchbay.pub/
+        GUI for: https://patchbay.pub/ Source: <a title="Github Source for Patch-Me" target="_blank" href="https://github.com/VictorioBerra/patch-me">https://github.com/VictorioBerra/patch-me</a>
         <footer>
           <small>
             <em>&mdash;By Tory Berra</em>
