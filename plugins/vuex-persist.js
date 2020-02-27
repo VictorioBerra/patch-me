86f6308b7c365741b37093a75bb8d2869628765c
Victorio Berra <toryberra@gmail.com>
2020-02-27 07:59:35 -0600

    Change persistance library.

--- plugins/vuex-persist.js
+++
@@ -1,9 +0,0 @@
-import VuexPersistence from 'vuex-persist'
-
-export default ({ store }) => {
-  window.onNuxtReady(() => {
-    new VuexPersistence({
-      storage: window.localStorage
-    }).plugin(store)
-  })
-}
